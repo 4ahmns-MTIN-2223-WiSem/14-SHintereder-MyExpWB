8f110b05797a983d282771148e24f39fc9fa5254
shintereder <100919783+shintereder@users.noreply.github.com>
2023-02-05 14:02:43 +0100

    Add files via upload

--- Assets/Scripts/Kugeln.cs
+++ Assets/Scripts/Kugeln.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Kugeln : MonoBehaviour
 
+
 {
     public GameObject decorationPrefab;
     private Transform treeTransform;
-    private GameObject currentDecoration;
+    private GameObject[] decorations;
 
     void Start()
     {
         treeTransform = gameObject.transform;
+        decorations = new GameObject[10]; // 10 ist eine beliebige Anzahl von Verzierungen, die Sie speichern möchten
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
+            int mouseButton = -1;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseButton = 0;
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                mouseButton = 1;
+            }
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
@@ -25,18 +38,43 @@ public class Kugeln : MonoBehaviour
             {
                 if (hit.transform == treeTransform)
                 {
-                    Vector3 decorationPosition = hit.point;
-                    decorationPosition.y += 0.5f;
-                    currentDecoration = Instantiate(decorationPrefab, decorationPosition, Quaternion.identity);
-                }
-                else if (hit.transform.gameObject == currentDecoration)
-                {
-                    Destroy(hit.transform.gameObject);
+                    switch (mouseButton)
+                    {
+                        case 0:
+                            // Platziere eine Verzierung
+                            Vector3 decorationPosition = hit.point;
+                            decorationPosition.y += 0.5f;
+                            GameObject decoration = Instantiate(decorationPrefab, decorationPosition, Quaternion.identity);
+
+                            // Speichere die Verzierung im Array
+                            for (int i = 0; i < decorations.Length; i++)
+                            {
+                                if (decorations[i] == null)
+                                {
+                                    decorations[i] = decoration;
+                                    break;
+                                }
+                            }
+                            break;
+                        case 1:
+                            // Entferne eine Verzierung
+                            for (int i = 0; i < decorations.Length; i++)
+                            {
+                                if (decorations[i] != null)
+                                {
+                                    Destroy(decorations[i]);
+                                    decorations[i] = null;
+                                    break;
+                                }
+                            }
+                            break;
+                    }
                 }
             }
         }
     }
 }
+
 
 
 
